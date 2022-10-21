5a7ccb00f1a6656e2fc8916e25cb629b8e7a5e24
sAyflort <neefe.saratan.life.life@gmail.com>
2022-10-21 12:18:50 +0300

    Добавлено security

--- src/main/java/ru/geekbrains/march/market/MarchMarketApplication.java
+++ src/main/java/ru/geekbrains/march/market/MarchMarketApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MarchMarketApplication {
 
+	//1) На фронте добавить в отделе корзины кнопку оформить заказ
+	//1.а) С фронта передать адрес и телефон покупателя
+	//2) Заказ может оформлять толькол вошедший пользователь (у3становите защиту)
+	//3) В ордерСервисе получаете текущую корзину и преобразуете ее к заказу
+	//4) Сохраняйте заказ с позициями заказа!! В БД
+	//5) ** На фронте можете показать список заказов юзера
+
 	public static void main(String[] args) {
 		SpringApplication.run(MarchMarketApplication.class, args);
 	}
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,4 +1,57 @@
-angular.module('market', []).controller('indexController', function ($scope, $http) {
+angular.module('market', ['ngStorage']).controller('indexController', function ($scope, $http, $localStorage) {
+    // Авторизация
+    $scope.tryToAuth = function () {
+        $http.post('http://localhost:8189/market/auth', $scope.user)
+            .then(function successCallback(response) {
+                if (response.data.token) {
+                    $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
+                    $localStorage.springMarketUser = {username: $scope.user.username, token: response.data.token};
+
+                    $scope.user.username = null;
+                    $scope.user.password = null;
+                }
+            }, function errorCallback(response) {
+            });
+    };
+
+    $scope.tryToLogout = function () {
+        $scope.clearUser();
+        $scope.user = null;
+    };
+
+    $scope.clearUser = function () {
+        delete $localStorage.springMarketUser;
+        $http.defaults.headers.common.Authorization = '';
+    };
+
+    $scope.isUserLoggedIn = function () {
+        return $localStorage.springMarketUser;
+    };
+
+    $scope.authCheck = function () {
+        $http.get('http://localhost:8189/market/auth/auth_check')
+            .then(function (response) {
+                console.log("анасик");
+                alert(response.data.value);
+            });
+    };
+
+    if ($localStorage.springMarketUser) {
+        try {
+            let jwt = $localStorage.springMarketUser.token;
+            let payload = JSON.parse(atob(jwt.split('.')[1]));
+            let currentTime = parseInt(new Date().getTime() / 1000);
+            if (currentTime > payload.exp) {
+                console.log("Token s expired!!!");
+                delete $localStorage.springMarketUser;
+                $http.defaults.headers.common.Authorization = '';
+            }
+        } catch (e) {
+        }
+        $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.springMarketUser.token;
+    }
+
+    // Таблица продуктов
     $scope.fillTable = function () {
         $http.get('http://localhost:8189/market/api/v1/products')
             .then(function (response) {
@@ -6,10 +59,11 @@ angular.module('market', []).controller('indexController', function ($scope, $ht
             });
     }
 
-    $scope.fillCartTable = function () {
-        $http.get('http://localhost:8189/market/api/v1/cart')
+    $scope.createNewProduct = function () {
+        $http.post('http://localhost:8189/market/api/v1/products', $scope.newProduct)
             .then(function (response) {
-                $scope.cart = response.data;
+                $scope.newProduct = null;
+                $scope.fillTable();
             });
     }
 
@@ -20,18 +74,18 @@ angular.module('market', []).controller('indexController', function ($scope, $ht
             });
     }
 
+    // Таблица корзины
+    $scope.fillCartTable = function () {
+        $http.get('http://localhost:8189/market/api/v1/cart')
+            .then(function (response) {
+                $scope.cart = response.data;
+            });
+    }
+
     $scope.deleteProductOfCart = function (id) {
         $http.delete('http://localhost:8189/market/api/v1/cart/delete/' + id)
             .then(function (response) {
                 $scope.fillCartTable();
-            });
-    }
-
-    $scope.createNewProduct = function () {
-        $http.post('http://localhost:8189/market/api/v1/products', $scope.newProduct)
-            .then(function (response) {
-                $scope.newProduct = null;
-                $scope.fillTable();
             });
     }
 
